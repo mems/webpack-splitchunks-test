0509b92cbf240ccfb08ea02da987690e68cd76eb
lnwu <wind2729@gmail.com>
2022-06-30 09:34:24 +0000

    init demo

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import TerserPlugin from "terser-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+const config = {
+  mode: "production",
+  devtool: false,
+  entry: "./src/index.js",
+  output: {
+    path: resolve(__dirname, "dist"),
+    chunkFilename: "[name].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: false,
+      template: "./index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        style1: {
+          test: /style1/,
+          enforce: true,
+        },
+        style2: {
+          test: /style2/,
+          enforce: true,
+        },
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          compress: true,
+          mangle: false,
+          format: {
+            comments: "all",
+            beautify: true,
+          },
+        },
+      }),
+    ],
+  },
+};
+
+export default config;
